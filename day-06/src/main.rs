a6c9d281f264e622d55d7b0c26734119493e7953
Ian <ianthomaseviston@gmail.com>
2023-01-03 22:10:24 -0600

    Finished day 6

--- day-06/src/main.rs
+++ day-06/src/main.rs
@@ -1,3 +1,24 @@
+use std::{fs, collections::HashSet};
+
 fn main() {
-    println!("Hello, world!");
+    let path = "resources/input.txt";
+    let stream = fs::read_to_string(path).expect("File not found");
+    // Part 1
+    let packet_start = find_first_unique_marker(&stream, 4).unwrap();
+    println!("Start of packet at {}", packet_start);
+
+    // Part 2
+    let message_start = find_first_unique_marker(&stream, 14).unwrap();
+    println!("Start of message at {}", message_start);
+}
+
+fn find_first_unique_marker(stream: &String, length: usize) -> Option<usize> {
+    for i in length..stream.len() {
+        let segment = &stream[(i - length)..i];
+        let char_set: HashSet<char> = HashSet::from_iter(segment.chars().into_iter());
+        if char_set.len() == length {
+            return Some(i);
+        }
+    }
+    return None;
 }
