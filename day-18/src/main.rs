f8ee98fc13e19ec9858cb842ad0485d468edd7aa
Ian <ianthomaseviston@gmail.com>
2023-01-19 16:59:56 -0500

    Started day 18

---
+++ day-18/src/main.rs
@@ -0,0 +1,7 @@
+use std::fs;
+
+fn main() {
+    let path = "resources/input.txt";
+    let contents = fs::read_to_string(path).expect("File not found");
+    let lines = contents.split("\n").map(|line| line.trim()).collect::<Vec<&str>>();
+}
