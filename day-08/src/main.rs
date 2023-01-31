90667ea135da6b1c476bc7fea0099157652aa002
Ian <ianthomaseviston@gmail.com>
2023-01-31 12:13:26 -0500

    Cleaned up and cargo fmt

--- day-08/src/main.rs
+++ day-08/src/main.rs
@@ -1,56 +1,128 @@
-use std::{fs, collections::{HashMap, HashSet}};
+use std::{
+    collections::{HashMap, HashSet},
+    fs,
+};
 
 fn main() {
     let path = "resources/input.txt";
     let contents = fs::read_to_string(path).expect("File not found");
     let lines = contents.split("\n").collect::<Vec<&str>>();
-    let trees = lines.iter().map(|line| {
-        line.trim().chars().map(|c| c.to_string().parse::<u8>().unwrap()).collect::<Vec<u8>>()
-    }).collect::<Vec<Vec<u8>>>();
-    
-    // Part 1 and 2
+    let trees = lines
+        .iter()
+        .map(|line| {
+            line.trim()
+                .chars()
+                .map(|c| c.to_string().parse::<u8>().unwrap())
+                .collect::<Vec<u8>>()
+        })
+        .collect::<Vec<Vec<u8>>>();
     let mut visible_trees: HashSet<(usize, usize)> = HashSet::new();
     let mut left_score_components: HashMap<(usize, usize), u16> = HashMap::new();
     let mut right_score_components: HashMap<(usize, usize), u16> = HashMap::new();
     let mut down_score_components: HashMap<(usize, usize), u16> = HashMap::new();
     let mut up_score_components: HashMap<(usize, usize), u16> = HashMap::new();
-    trees.iter().enumerate().for_each(|(i, tree_line)| {
+    for (i, tree_line) in trees.iter().enumerate() {
         let mut tallest: Option<u8> = None;
         let mut distance_to_nearest_tree_of_at_least_height: Vec<Option<u16>> = vec![None; 10];
-        tree_line.iter().enumerate().for_each(|(j, tree)| {
-            tallest = Some(update_tallest_tree(tallest, *tree, &mut visible_trees, i, j));
-            update_score_component(&mut left_score_components, &mut distance_to_nearest_tree_of_at_least_height, *tree, i, j, j as u16);
+        tree_line.iter().enumerate().for_each(|(j, &tree)| {
+            tallest = Some(update_visible_trees(
+                tallest,
+                tree,
+                &mut visible_trees,
+                i,
+                j,
+            ));
+            update_score_component(
+                &mut left_score_components,
+                &mut distance_to_nearest_tree_of_at_least_height,
+                tree,
+                i,
+                j,
+                j as u16,
+            );
         });
         let mut tallest: Option<u8> = None;
         let mut distance_to_nearest_tree_of_at_least_height: Vec<Option<u16>> = vec![None; 10];
-        tree_line.iter().rev().enumerate().for_each(|(j_rev, tree)| {
-            let j = tree_line.len() - j_rev - 1;
-            tallest = Some(update_tallest_tree(tallest, *tree, &mut visible_trees, i, j));
-            update_score_component(&mut right_score_components, &mut distance_to_nearest_tree_of_at_least_height, *tree, i, j, j_rev as u16);
-        });
-    });
-    (0..trees[0].len()).for_each(|j| {
-        let tree_line = trees.iter().map(|tree_row| tree_row[j]).collect::<Vec<u8>>();
+        tree_line
+            .iter()
+            .rev()
+            .enumerate()
+            .for_each(|(j_rev, &tree)| {
+                let j = tree_line.len() - j_rev - 1;
+                tallest = Some(update_visible_trees(
+                    tallest,
+                    tree,
+                    &mut visible_trees,
+                    i,
+                    j,
+                ));
+                update_score_component(
+                    &mut right_score_components,
+                    &mut distance_to_nearest_tree_of_at_least_height,
+                    tree,
+                    i,
+                    j,
+                    j_rev as u16,
+                );
+            });
+    }
+    for j in 0..trees[0].len() {
+        let tree_line = trees
+            .iter()
+            .map(|tree_row| tree_row[j])
+            .collect::<Vec<u8>>();
         let mut tallest: Option<u8> = None;
         let mut distance_to_nearest_tree_of_at_least_height: Vec<Option<u16>> = vec![None; 10];
         tree_line.iter().enumerate().for_each(|(i, tree)| {
-            tallest = Some(update_tallest_tree(tallest, *tree, &mut visible_trees, i, j));
-            update_score_component(&mut down_score_components, &mut distance_to_nearest_tree_of_at_least_height, *tree, i, j, i as u16);
+            tallest = Some(update_visible_trees(
+                tallest,
+                *tree,
+                &mut visible_trees,
+                i,
+                j,
+            ));
+            update_score_component(
+                &mut down_score_components,
+                &mut distance_to_nearest_tree_of_at_least_height,
+                *tree,
+                i,
+                j,
+                i as u16,
+            );
         });
         let mut tallest: Option<u8> = None;
         let mut distance_to_nearest_tree_of_at_least_height: Vec<Option<u16>> = vec![None; 10];
-        tree_line.iter().rev().enumerate().for_each(|(i_rev, tree)| {
-            let i = tree_line.len() - i_rev - 1;
-            tallest = Some(update_tallest_tree(tallest, *tree, &mut visible_trees, i, j));
-            update_score_component(&mut up_score_components, &mut distance_to_nearest_tree_of_at_least_height, *tree, i, j, i_rev as u16);
-        });
-    });
+        tree_line
+            .iter()
+            .rev()
+            .enumerate()
+            .for_each(|(i_rev, tree)| {
+                let i = tree_line.len() - i_rev - 1;
+                tallest = Some(update_visible_trees(
+                    tallest,
+                    *tree,
+                    &mut visible_trees,
+                    i,
+                    j,
+                ));
+                update_score_component(
+                    &mut up_score_components,
+                    &mut distance_to_nearest_tree_of_at_least_height,
+                    *tree,
+                    i,
+                    j,
+                    i_rev as u16,
+                );
+            });
+    }
+
+    // Part 1
     println!("Total visible trees: {}", visible_trees.len());
 
     // Part 2
     let mut highest_score = 0;
-    (0..trees.len()).for_each(|i| {
-        (0..trees[0].len()).for_each(|j| {
+    for i in 0..trees.len() {
+        for j in 0..trees[0].len() {
             let left = *left_score_components.get(&(i, j)).unwrap_or(&0);
             let right = *right_score_components.get(&(i, j)).unwrap_or(&0);
             let down = *down_score_components.get(&(i, j)).unwrap_or(&0);
@@ -59,35 +131,46 @@ fn main() {
             if score > highest_score {
                 highest_score = score;
             }
-        });
-    });
+        }
+    }
     println!("Highest score: {}", highest_score);
 }
 
-fn update_tallest_tree(tallest: Option<u8>, tree: u8, visible_trees: &mut HashSet<(usize, usize)>, i: usize, j: usize) -> u8 {
+fn update_visible_trees(
+    tallest: Option<u8>,
+    tree: u8,
+    visible_trees: &mut HashSet<(usize, usize)>,
+    i: usize,
+    j: usize,
+) -> u8 {
     if let Some(tallest_val) = tallest {
         if tree > tallest_val {
             visible_trees.insert((i, j));
-        }
-        else {
+        } else {
             return tallest_val;
         }
-    }
-    else {
+    } else {
         visible_trees.insert((i, j));
     }
     return tree;
 }
 
-fn update_score_component(score_component: &mut HashMap<(usize, usize), u16>, distance_to_nearest_tree_of_at_least_height: &mut Vec<Option<u16>>, tree: u8, i: usize, j: usize, default: u16) {
+fn update_score_component(
+    score_component: &mut HashMap<(usize, usize), u16>,
+    distance_to_nearest_tree_of_at_least_height: &mut Vec<Option<u16>>,
+    tree: u8,
+    i: usize,
+    j: usize,
+    default: u16,
+) {
     let comp = distance_to_nearest_tree_of_at_least_height[tree as usize].unwrap_or(default);
     score_component.insert((i, j), comp);
-    (0..((tree + 1) as usize)).for_each(|k| {
+    for k in 0..(tree + 1) as usize {
         distance_to_nearest_tree_of_at_least_height[k] = Some(1);
-    });
-    (((tree + 1) as usize)..10).for_each(|k| {
+    }
+    for k in (tree + 1) as usize..10 {
         if let Some(distance) = distance_to_nearest_tree_of_at_least_height[k] {
             distance_to_nearest_tree_of_at_least_height[k] = Some(distance + 1);
         }
-    });
+    }
 }
