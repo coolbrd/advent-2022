eef17dcdd3ea05021a4216ac8a05e70a40eac8b8
Ian <ianthomaseviston@gmail.com>
2023-02-01 21:03:42 -0500

    Cleaned up and cargo fmt

--- day-18/src/main.rs
+++ day-18/src/main.rs
@@ -1,16 +1,36 @@
-use std::{fs, collections::HashSet};
+use std::{collections::HashSet, fs};
 
-const FACE_OFFSETS: [(i8, i8, i8); 6] = [(-1, 0, 0), (1, 0, 0), (0, -1, 0), (0, 1, 0), (0, 0, -1), (0, 0, 1)];
+const FACE_OFFSETS: [Pos; 6] = [
+    (-1, 0, 0),
+    (1, 0, 0),
+    (0, -1, 0),
+    (0, 1, 0),
+    (0, 0, -1),
+    (0, 0, 1),
+];
+
+type PosComp = i8;
+
+type Pos = (PosComp, PosComp, PosComp);
 
 fn main() {
     let path = "resources/input.txt";
     let contents = fs::read_to_string(path).expect("File not found");
-    let lines = contents.split("\n").map(|line| line.trim()).collect::<Vec<&str>>();
-    let cubes = lines.iter().map(|line| {
-        let components = line.split(",").map(|comp| comp.parse().unwrap()).collect::<Vec<i8>>();
-        (components[0], components[1], components[2])
-    }).collect::<Vec<(i8, i8, i8)>>();
-    let cubes: HashSet<(i8, i8, i8)> = cubes.into_iter().collect();
+    let lines = contents
+        .split("\n")
+        .map(|line| line.trim())
+        .collect::<Vec<&str>>();
+    let cubes = lines
+        .iter()
+        .map(|line| {
+            let components = line
+                .split(",")
+                .map(|comp| comp.parse().unwrap())
+                .collect::<Vec<i8>>();
+            (components[0], components[1], components[2])
+        })
+        .collect::<Vec<Pos>>();
+    let cubes = cubes.into_iter().collect();
 
     // Part 1
     let total_exposed_faces = calculate_total_exposed_faces(&cubes);
@@ -22,12 +42,13 @@ fn main() {
     println!("Outside exposed faces: {}", outside_exposed_faces);
 }
 
-fn calculate_total_exposed_faces(cubes: &HashSet<(i8, i8, i8)>) -> usize {
+fn calculate_total_exposed_faces(cubes: &HashSet<Pos>) -> usize {
     let mut total_exposed_faces = 0;
     for cube in cubes {
         let mut exposed_faces = 0;
         for offset in &FACE_OFFSETS {
-            if !cubes.contains(&(cube.0 + offset.0, cube.1 + offset.1, cube.2 + offset.2)) {
+            let offset_pos = (cube.0 + offset.0, cube.1 + offset.1, cube.2 + offset.2);
+            if !cubes.contains(&offset_pos) {
                 exposed_faces += 1;
             }
         }
@@ -36,9 +57,9 @@ fn calculate_total_exposed_faces(cubes: &HashSet<(i8, i8, i8)>) -> usize {
     total_exposed_faces
 }
 
-fn calculate_outside_air_cubes(cubes: &HashSet<(i8, i8, i8)>) -> HashSet<(i8, i8, i8)> {
-    let mut min_xyz = (i8::MAX, i8::MAX, i8::MAX);
-    let mut max_xyz = (i8::MIN, i8::MIN, i8::MIN);
+fn calculate_outside_air_cubes(cubes: &HashSet<Pos>) -> HashSet<Pos> {
+    let mut min_xyz = (PosComp::MAX, PosComp::MAX, PosComp::MAX);
+    let mut max_xyz = (PosComp::MIN, PosComp::MIN, PosComp::MIN);
     for cube in cubes {
         min_xyz.0 = min_xyz.0.min(cube.0 - 1);
         min_xyz.1 = min_xyz.1.min(cube.1 - 1);
@@ -48,26 +69,43 @@ fn calculate_outside_air_cubes(cubes: &HashSet<(i8, i8, i8)>) -> HashSet<(i8, i8
         max_xyz.2 = max_xyz.2.max(cube.2 + 1);
     }
     let mut air_cube_queue = vec![min_xyz];
-    let mut outside_air_cubes: HashSet<(i8, i8, i8)> = HashSet::new();
+    let mut outside_air_cubes = HashSet::new();
     while !air_cube_queue.is_empty() {
         let current_air_cube = air_cube_queue.pop().unwrap();
-        let adjacent_air_cubes = FACE_OFFSETS.iter().map(|offset| {
-            (current_air_cube.0 + offset.0, current_air_cube.1 + offset.1, current_air_cube.2 + offset.2)
-        }).filter(|adjacent_cube| {
-            !&cubes.contains(adjacent_cube) && !outside_air_cubes.contains(adjacent_cube) && !point_is_outside_bounds(adjacent_cube, &min_xyz, &max_xyz)
-        }).collect::<Vec<(i8, i8, i8)>>();
+        let adjacent_air_cubes = FACE_OFFSETS
+            .iter()
+            .map(|offset| {
+                (
+                    current_air_cube.0 + offset.0,
+                    current_air_cube.1 + offset.1,
+                    current_air_cube.2 + offset.2,
+                )
+            })
+            .filter(|adjacent_cube| {
+                !&cubes.contains(adjacent_cube)
+                    && !outside_air_cubes.contains(adjacent_cube)
+                    && !point_is_outside_bounds(adjacent_cube, &min_xyz, &max_xyz)
+            })
+            .collect::<Vec<Pos>>();
         air_cube_queue.extend(adjacent_air_cubes);
         outside_air_cubes.insert(current_air_cube);
     }
     outside_air_cubes
 }
 
-fn calculate_faces_exposed_to_outside_air(cubes: &HashSet<(i8, i8, i8)>, outside_air_cubes: &HashSet<(i8, i8, i8)>) -> usize {
+fn calculate_faces_exposed_to_outside_air(
+    cubes: &HashSet<Pos>,
+    outside_air_cubes: &HashSet<Pos>,
+) -> usize {
     let mut total_exposed_faces = 0;
     for cube in cubes {
         let mut exposed_faces = 0;
         for offset in &FACE_OFFSETS {
-            if outside_air_cubes.contains(&(cube.0 + offset.0, cube.1 + offset.1, cube.2 + offset.2)) {
+            if outside_air_cubes.contains(&(
+                cube.0 + offset.0,
+                cube.1 + offset.1,
+                cube.2 + offset.2,
+            )) {
                 exposed_faces += 1;
             }
         }
@@ -76,6 +114,11 @@ fn calculate_faces_exposed_to_outside_air(cubes: &HashSet<(i8, i8, i8)>, outside
     total_exposed_faces
 }
 
-fn point_is_outside_bounds(point: &(i8, i8, i8), min_xyz: &(i8, i8, i8), max_xyz: &(i8, i8, i8)) -> bool {
-    point.0 < min_xyz.0 || point.0 > max_xyz.0 || point.1 < min_xyz.1 || point.1 > max_xyz.1 || point.2 < min_xyz.2 || point.2 > max_xyz.2
+fn point_is_outside_bounds(point: &Pos, min_xyz: &Pos, max_xyz: &Pos) -> bool {
+    point.0 < min_xyz.0
+        || point.0 > max_xyz.0
+        || point.1 < min_xyz.1
+        || point.1 > max_xyz.1
+        || point.2 < min_xyz.2
+        || point.2 > max_xyz.2
 }
